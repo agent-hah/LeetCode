242272c5694745f1baa3125ea3c55b3da9eb8478
Ashmit Handoo <ashmithandoo@outlook.com>
2025-11-23 18:51:49 -0500

    fixed problem

--- three_sum_closest.py
+++ three_sum_closest.py
@@ -17,14 +17,8 @@ def threeSumClosest(nums: list[int], target: int) -> int:
             if total < target:
                 j += 1
 
-                while (j < k and nums[j] == nums[j - 1]):
-                    j += 1
-
             elif total > target:
                 k -= 1
-
-                while (j < k and nums[k] == nums[k + 1]):
-                    k -=1
 
             else:
                 return target
