ccf9ba5f5780968e656a72ac67f88fc8385c945f
Ashmit Handoo <ashmithandoo@outlook.com>
2025-11-27 19:00:18 -0500

    added comments to isPowerOfFour solution

--- problems/bitManipulation/isPowerOfFour.py
+++ problems/bitManipulation/isPowerOfFour.py
@@ -1,4 +1,9 @@
 def isPowerOfFour(n: int) -> bool:
+    # 4 = 100
+    # 1 = 001
+    # 16 = 10000
+    # 64 = 1000000
+    # 6 = 110
     return (n > 0) and (n & (n-1) == 0) and (bin(n - 1)[2::].count("1") % 2 == 0)
 
 print(isPowerOfFour(4 ** 1000000000))
